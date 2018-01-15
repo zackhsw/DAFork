21385bf3673fb2868184bb1edb9957d34bd27c57
zack <3233313578@qq.com>
2018-01-15 18:08:37 +0800

    rename second~

--- drctools/regitster_img.py
+++ drctools/regitster_img.py
@@ -20,14 +20,12 @@ def get_md5(src):
 def rename_files():
     path = r'E:\迅雷下载\1'
     for file in os.listdir(path):
-        print(file)
+        # print(file)
         if os.path.isfile(os.path.join(path, file)) == True:
-            if file.find('.') < 0:
-                newname = uuid.uuid1()
-                # os.rename(os.path.join(path, file), os.path.join(path, newname))
-                os.rename(file, newname)
-                print(file, 'ok')
-                # print file.split('.')[-1]
+            newname = str(uuid.uuid1())
+            os.rename(os.path.join(path, file), os.path.join(path, newname) + '.jpeg')
+            # print(file, 'ok')
+            # print file.split('.')[-1]
 
 
 def write_to_text():
@@ -35,13 +33,12 @@ def write_to_text():
     with open(r'E:\dataset.txt', 'w') as f:
         file_dir = r'E:\迅雷下载'
         for root, dirs, files in os.walk(file_dir):
-            print(dirs)
             for name in files:
                 f.writelines(
-                    '86.747.028/' + str(uuid.uuid1()) + '  '
-                    + name + '  '
+                    "".join(dirs) + '  '
+                    + str(uuid.uuid1()) + '.jpeg' + '  '
                     + "by LiJuan" + '  '
-                    + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '  '
+                    # + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '  '
                     + str(datetime.utcnow()) + '  '
                     + os.path.join(file_dir, name) + '\n')
                 # list_set = [get_md5(name),
@@ -54,4 +51,4 @@ def write_to_text():
 
 if __name__ == "__main__":
     rename_files()
-    # write_to_text()
+    write_to_text()
